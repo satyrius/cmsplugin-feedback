a03ddccf32334e68c39dba9a0c62ee08a44cf927
Anton Egorov <anton.egoroff@gmail.com>
2014-11-10 04:25:07 +0300

    delete useless templatetags

--- cmsplugin_feedback/templatetags/feedback_form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django import template
-register = template.Library()
-
-
-@register.filter()
-def is_checkbox(field):
-    return isinstance(field.field, forms.BooleanField)
