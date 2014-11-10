79db5e170fc72ad6aeff589462963e1a5a638455
Anton Egorov <anton.egoroff@gmail.com>
2014-11-10 17:45:33 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,61 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+from cmsplugin_feedback import __version__
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='cmsplugin-feedback',
+    version=__version__,
+    description='Feedback form plugin for Django CMS',
+    author='Anton Egorov',
+    author_email='anton.egoroff@gmail.com',
+    url='https://github.com/satyrius/cmsplugin-feedback',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    classifiers=CLASSIFIERS,
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django-cms',
+        'django-simple-captcha>=0.4.1',
+    ],
+    tests_require=['tox>=1.8'],
+    cmdclass={'test': Tox},
+    zip_safe=False,
+)
