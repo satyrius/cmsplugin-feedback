bd938d66c65ff117ba7f4eca4f7f41c7bd1b262b
Anton Egorov <anton.egoroff@gmail.com>
2014-11-10 03:54:23 +0300

    test with nose

--- tests/django_settings.py
+++ tests/django_settings.py
@@ -1,3 +1,10 @@
+LANGUAGE_CODE = 'en'
+SECRET_KEY = 'ji2r2iGkZqJVbWDhXrgDKDR2qG#mmtvBZXPXDugA4H)KFLwLHy'
+SITE_ID = 1
+SOUTH_TESTS_MIGRATE = True
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ['--nologcapture']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -9,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.auth',
     'django.contrib.sites',
+    'django_nose',
     'mptt',
     'cms',
     'cmsplugin_feedback',
@@ -18,7 +26,3 @@ INSTALLED_APPS = [
 TEMPLATE_CONTEXT_PROCESSORS = [
     'django.core.context_processors.request',
 ]
-
-SECRET_KEY = 'ji2r2iGkZqJVbWDhXrgDKDR2qG#mmtvBZXPXDugA4H)KFLwLHy'
-
-SOUTH_TESTS_MIGRATE = True
