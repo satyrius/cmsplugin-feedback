5b4c1835f20d9a6b6c74d0da2ed56c1945222750
Anton Egorov <anton.egoroff@gmail.com>
2014-11-12 00:44:49 +0300

    render plugins with SekizaiContext in tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,6 +3,7 @@ from cms.api import add_plugin
 from cms.models import Placeholder
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from sekizai.context import SekizaiContext
 
 from cmsplugin_feedback.cms_plugins import FeedbackPlugin, \
     DEFAULT_FORM_FIELDS_ID, DEFAULT_FORM_CLASS
@@ -21,6 +22,9 @@ class FeedbackPluginTests(TestCase):
             **kwargs)
         return model_instance
 
+    def render(self, plugin):
+        return plugin.render_plugin(SekizaiContext())
+
     def test_plugin_context(self):
         model = self.add_plugin()
         plugin = model.get_plugin_class_instance()
@@ -36,7 +40,7 @@ class FeedbackPluginTests(TestCase):
     def test_form_title(self):
         title = 'Feedback Form'
         plugin = self.add_plugin(title=title)
-        html = plugin.render_plugin({})
+        html = self.render(plugin)
         soup = BeautifulSoup(html)
         self.assertEqual(soup.h1.string, title)
 
@@ -45,7 +49,7 @@ class FeedbackPluginTests(TestCase):
         self.assertTrue(plugin.submit)
         default = plugin._meta.get_field_by_name('submit')[0].default
         self.assertEqual(plugin.submit, default)
-        html = plugin.render_plugin({})
+        html = self.render(plugin)
         soup = BeautifulSoup(html)
         self.assertEqual(soup.find(type='submit').string, default)
 
@@ -55,13 +59,13 @@ class FeedbackPluginTests(TestCase):
         default = plugin._meta.get_field_by_name('submit')[0].default
         self.assertNotEqual(text, default)
         self.assertEqual(plugin.submit, text)
-        html = plugin.render_plugin({})
+        html = self.render(plugin)
         soup = BeautifulSoup(html)
         self.assertEqual(soup.find(type='submit').string, text)
 
     def test_form_action_url(self):
         plugin = self.add_plugin()
-        html = plugin.render_plugin({})
+        html = self.render(plugin)
         soup = BeautifulSoup(html)
         self.assertEqual(
             soup.form['action'],
