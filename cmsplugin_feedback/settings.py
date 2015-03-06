b519c4d1eaa1ea6a0f4555234a5ecb702dc1619a
Anton Egorov <anton.egoroff@gmail.com>
2015-03-06 17:59:27 +0300

    remove deprecated settings

--- cmsplugin_feedback/settings.py
+++ cmsplugin_feedback/settings.py
@@ -1,15 +1,6 @@
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-
-DEFAULT_FORM_FIELDS_ID = 'contact-%s'
-FORM_FIELDS_ID = getattr(
-    settings, 'CMS_FEEDBACK_FORM_FIELD_ID', DEFAULT_FORM_FIELDS_ID)
-
-DEFAULT_FORM_CLASS = 'contact-form'
-FORM_CLASS = getattr(
-    settings, 'CMS_FEEDBACK_FORM_CSS_CLASS', DEFAULT_FORM_CLASS)
-
 NOTIFY_MANAGERS = getattr(
     settings, 'CMS_FEEDBACK_NOTIFY_MANAGERS', True)
 
